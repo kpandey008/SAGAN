4ca5ec0c731a494d9fb19aee0904f338e86cb3cd
Kushagra Pandey <kpandeyce008@gmail.com>
2021-06-09 09:35:20 +0530

    Added variants of gen and disc models

--- models/discriminator.py
+++ models/discriminator.py
@@ -4,6 +4,77 @@ import torch.nn as nn
 from torch.nn.utils import spectral_norm
 
 
+class DiscriminatorSNBlock(nn.Module):
+    # A generator block to upsample the input by a factor of 2
+    def __init__(self, in_channels, out_channels):
+        super(DiscriminatorSNBlock, self).__init__()
+        self.relu = nn.ReLU()
+        self.downsample = nn.AvgPool2d(2)
+
+        self.conv_module = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_channels, out_channels, 3, 1, padding=1)),
+            nn.ReLU(),
+            spectral_norm(nn.Conv2d(out_channels, out_channels, 3, 1, padding=1)),
+            nn.ReLU(),
+        )
+
+        self.residual_conv = spectral_norm(nn.Conv2d(in_channels, out_channels, 1, 1))
+
+    def forward(self, x):
+        identity = x
+
+        # SN Conv and Downsample
+        x = self.conv_module(x)
+        x = self.downsample(x)
+
+        # Residual connection
+        return x + self.residual_conv(self.downsample(identity))
+
+
+class SAGANDiscriminator(nn.Module):
+    def __init__(self, n_heads=1):
+        super(SAGANDiscriminator, self).__init__()
+        self.n_heads = n_heads
+
+        self.block1 = DiscriminatorSNBlock(3, 64)  # 32 x 32
+        self.block2 = DiscriminatorSNBlock(64, 128)  # 16 x 16
+        self.block3 = DiscriminatorSNBlock(128, 256)  # 8 x 8
+        self.block4 = DiscriminatorSNBlock(256, 512)  # 4 x 4
+
+        self.flatten = nn.Flatten()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.clf = nn.Linear(512, 1, bias=False)
+
+        self.attn1 = nn.MultiheadAttention(64, num_heads=self.n_heads)
+        self.alpha = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+
+    def forward(self, x):
+        # Forward
+        out = self.block1(x)
+
+        #### Attention 1 ####
+        identity = out
+        B, C, H, W = out.shape
+        q_1 = out.view(H * W, B, C)
+        k_1 = out.view(H * W, B, C)
+        v_1 = out.view(H * W, B, C)
+        out, attn_map_1 = self.attn1(q_1, k_1, v_1)
+        out = out.view(B, C, H, W)
+
+        # Residual connection
+        out = identity + self.alpha * out
+
+        # Forward (contd.)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+
+        out = self.avg_pool(out)
+        out = self.flatten(out)
+
+        return self.clf(out).squeeze(), attn_map_1
+
+
 class Discriminator(nn.Module):
     def __init__(self, conv_dim=64, n_heads=1):
         super(Discriminator, self).__init__()
--- models/generator.py
+++ models/generator.py
@@ -4,8 +4,91 @@ import torch.nn as nn
 from torch.nn.utils import spectral_norm
 
 
+class GeneratorSNBlock(nn.Module):
+    # A generator block to upsample the input by a factor of 2
+    def __init__(self, in_channels, out_channels):
+        super(GeneratorSNBlock, self).__init__()
+        self.relu = nn.ReLU()
+        self.upsample = nn.Upsample(scale_factor=2, mode="nearest")
+
+        self.conv_module = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_channels, out_channels, 3, 1, padding=1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            spectral_norm(nn.Conv2d(out_channels, out_channels, 3, 1, padding=1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+        self.residual_conv = spectral_norm(nn.Conv2d(in_channels, out_channels, 1, 1))
+
+    def forward(self, x):
+        identity = x
+
+        # Upsample and SN Conv
+        x = self.upsample(x)
+        x = self.conv_module(x)
+
+        # Residual connection
+        return x + self.residual_conv(self.upsample(identity))
+
+
+class SAGANGenerator(nn.Module):
+    def __init__(self, z_dim, in_channels, n_heads=1):
+        super(SAGANGenerator, self).__init__()
+        self.z_dim = z_dim
+        self.in_channels = in_channels
+        self.n_heads = n_heads
+
+        self.z_linear = spectral_norm(
+            nn.Linear(self.z_dim, 4 * 4 * self.in_channels, bias=False)
+        )
+
+        self.block1 = GeneratorSNBlock(self.in_channels, 256)  # 8 x 8
+        self.block2 = GeneratorSNBlock(256, 128)  # 16 x 16
+        self.block3 = GeneratorSNBlock(128, 64)  # 32 x 32
+        self.block4 = GeneratorSNBlock(64, 64)  # 64 x 64
+
+        self.attn1 = nn.MultiheadAttention(64, num_heads=self.n_heads)
+        self.alpha = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+
+        self.last = nn.Sequential(
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 3, 3, 1, padding=1),
+        )
+
+    def forward(self, z):
+        # Reshape z
+        z = self.z_linear(z)
+        z = z.view(z.size(0), self.in_channels, 4, 4)
+
+        # Forward
+        out = self.block1(z)
+        out = self.block2(out)
+        out = self.block3(out)
+
+        #### Attention 1 ####
+        identity = out
+        B, C, H, W = out.shape
+        q_1 = out.view(H * W, B, C)
+        k_1 = out.view(H * W, B, C)
+        v_1 = out.view(H * W, B, C)
+        out, attn_map_1 = self.attn1(q_1, k_1, v_1)
+        out = out.view(B, C, H, W)
+
+        # Residual connection
+        out = identity + self.alpha * out
+
+        # Forward (contd.)
+        out = self.block4(out)
+        out = self.last(out)
+
+        return torch.tanh(out), attn_map_1
+
+
 class Generator(nn.Module):
-    def __init__(self, z_dim=100, conv_dim=64, n_heads=1):
+    def __init__(self, z_dim=128, conv_dim=64, n_heads=1):
         super(Generator, self).__init__()
         self.z_dim = z_dim
         self.conv_dim = conv_dim
@@ -18,19 +101,25 @@ class Generator(nn.Module):
         )
 
         self.layer2 = nn.Sequential(
-            spectral_norm(nn.ConvTranspose2d(8 * self.conv_dim, 4 * self.conv_dim, 4, 2, 1)),
+            spectral_norm(
+                nn.ConvTranspose2d(8 * self.conv_dim, 4 * self.conv_dim, 4, 2, 1)
+            ),
             nn.BatchNorm2d(4 * self.conv_dim),
             nn.ReLU(),
         )
 
         self.layer3 = nn.Sequential(
-            spectral_norm(nn.ConvTranspose2d(4 * self.conv_dim, 2 * self.conv_dim, 4, 2, 1)),
+            spectral_norm(
+                nn.ConvTranspose2d(4 * self.conv_dim, 2 * self.conv_dim, 4, 2, 1)
+            ),
             nn.BatchNorm2d(2 * self.conv_dim),
             nn.ReLU(),
         )
 
         self.layer4 = nn.Sequential(
-            spectral_norm(nn.ConvTranspose2d(2 * self.conv_dim, self.conv_dim, 4, 2, 1)),
+            spectral_norm(
+                nn.ConvTranspose2d(2 * self.conv_dim, self.conv_dim, 4, 2, 1)
+            ),
             nn.BatchNorm2d(self.conv_dim),
             nn.ReLU(),
         )
@@ -39,7 +128,7 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(self.conv_dim, self.conv_dim, 4, 2, 1),
             nn.ReLU(),
             nn.ConvTranspose2d(self.conv_dim, 3, 4, 2, 1),
-            nn.ReLU()
+            nn.ReLU(),
         )
 
         self.attn1 = nn.MultiheadAttention(2 * self.conv_dim, num_heads=self.n_heads)
