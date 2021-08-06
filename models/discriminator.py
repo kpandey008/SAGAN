8f32a78a42c0d6470d8682ecac939aca41335c26
Kushagra Pandey <kpandeyce008@gmail.com>
2021-08-06 08:32:07 +0530

    Removed obsolete models

--- models/discriminator.py
+++ models/discriminator.py
@@ -36,21 +36,23 @@ class SAGANDiscriminator(nn.Module):
         super(SAGANDiscriminator, self).__init__()
         self.n_heads = n_heads
 
-        self.block1 = DiscriminatorSNBlock(3, 64)  # 32 x 32
+        self.block0 = DiscriminatorSNBlock(3, 32)  # 64 x 64
+        self.block1 = DiscriminatorSNBlock(32, 64)  # 32 x 32
         self.block2 = DiscriminatorSNBlock(64, 128)  # 16 x 16
         self.block3 = DiscriminatorSNBlock(128, 256)  # 8 x 8
-        self.block4 = DiscriminatorSNBlock(256, 512)  # 4 x 4
+        self.block4 = DiscriminatorSNBlock(256, 256)  # 4 x 4
 
         self.flatten = nn.Flatten()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.clf = nn.Linear(512, 1, bias=False)
+        self.clf = nn.Linear(256, 1, bias=False)
 
         self.attn1 = nn.MultiheadAttention(64, num_heads=self.n_heads)
         self.alpha = nn.Parameter(torch.tensor(0.0), requires_grad=True)
 
     def forward(self, x):
         # Forward
-        out = self.block1(x)
+        out = self.block0(x)
+        out = self.block1(out)
 
         #### Attention 1 ####
         identity = out
@@ -73,72 +75,3 @@ class SAGANDiscriminator(nn.Module):
         out = self.flatten(out)
 
         return self.clf(out).squeeze(), attn_map_1
-
-
-class Discriminator(nn.Module):
-    def __init__(self, conv_dim=64, n_heads=1):
-        super(Discriminator, self).__init__()
-        self.conv_dim = conv_dim
-        self.n_heads = n_heads
-
-        self.layer1 = nn.Sequential(
-            spectral_norm(nn.Conv2d(3, self.conv_dim, 4, 2, 1)),
-            nn.LeakyReLU(0.1),
-        )
-
-        self.layer2 = nn.Sequential(
-            spectral_norm(nn.Conv2d(self.conv_dim, self.conv_dim * 2, 4, 2, 1)),
-            nn.LeakyReLU(0.1),
-        )
-
-        self.layer3 = nn.Sequential(
-            spectral_norm(nn.Conv2d(2 * self.conv_dim, 4 * self.conv_dim, 4, 2, 1)),
-            nn.LeakyReLU(0.1),
-        )
-
-        self.layer4 = nn.Sequential(
-            spectral_norm(nn.Conv2d(4 * self.conv_dim, 8 * self.conv_dim, 4, 2, 1)),
-            nn.LeakyReLU(0.1),
-        )
-
-        self.output = nn.Conv2d(8 * self.conv_dim, 1, 4)
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-
-        self.attn1 = nn.MultiheadAttention(4 * self.conv_dim, num_heads=self.n_heads)
-        self.alpha = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        self.attn2 = nn.MultiheadAttention(8 * self.conv_dim, num_heads=self.n_heads)
-        self.beta = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-
-        #### Attention 1 ####
-        identity = out
-        B, C, H, W = out.shape
-        q_1 = out.view(H * W, B, C)
-        k_1 = out.view(H * W, B, C)
-        v_1 = out.view(H * W, B, C)
-        out, attn_map_1 = self.attn1(q_1, k_1, v_1)
-        out = out.view(B, C, H, W)
-
-        # Residual connection
-        out = identity + self.alpha * out
-
-        out = self.layer4(out)
-
-        #### Attention 2 ####
-        identity = out
-        B, C, H, W = out.shape
-        q_2 = out.view(H * W, B, C)
-        k_2 = out.view(H * W, B, C)
-        v_2 = out.view(H * W, B, C)
-        out, attn_map_2 = self.attn2(q_2, k_2, v_2)
-        out = out.view(B, C, H, W)
-
-        # Residual connection
-        out = identity + self.beta * out
-        out = self.avg_pool(self.output(out))
-
-        return out.squeeze(), attn_map_1, attn_map_2
--- models/generator.py
+++ models/generator.py
@@ -47,15 +47,16 @@ class SAGANGenerator(nn.Module):
         self.block1 = GeneratorSNBlock(self.in_channels, 256)  # 8 x 8
         self.block2 = GeneratorSNBlock(256, 128)  # 16 x 16
         self.block3 = GeneratorSNBlock(128, 64)  # 32 x 32
-        self.block4 = GeneratorSNBlock(64, 64)  # 64 x 64
+        self.block4 = GeneratorSNBlock(64, 32)  # 64 x 64
+        self.block5 = GeneratorSNBlock(32, 16)  # 128 x 128
 
         self.attn1 = nn.MultiheadAttention(64, num_heads=self.n_heads)
         self.alpha = nn.Parameter(torch.tensor(0.0), requires_grad=True)
 
         self.last = nn.Sequential(
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(64, 3, 3, 1, padding=1),
+            nn.Conv2d(32, 3, 3, 1, padding=1),
         )
 
     def forward(self, z):
@@ -82,89 +83,7 @@ class SAGANGenerator(nn.Module):
 
         # Forward (contd.)
         out = self.block4(out)
+        out = self.block5(out)
         out = self.last(out)
 
         return torch.tanh(out), attn_map_1
-
-
-class Generator(nn.Module):
-    def __init__(self, z_dim=128, conv_dim=64, n_heads=1):
-        super(Generator, self).__init__()
-        self.z_dim = z_dim
-        self.conv_dim = conv_dim
-        self.n_heads = n_heads
-
-        self.layer1 = nn.Sequential(
-            spectral_norm(nn.ConvTranspose2d(self.z_dim, 8 * self.conv_dim, 4, 2, 1)),
-            nn.BatchNorm2d(8 * self.conv_dim),
-            nn.ReLU(),
-        )
-
-        self.layer2 = nn.Sequential(
-            spectral_norm(
-                nn.ConvTranspose2d(8 * self.conv_dim, 4 * self.conv_dim, 4, 2, 1)
-            ),
-            nn.BatchNorm2d(4 * self.conv_dim),
-            nn.ReLU(),
-        )
-
-        self.layer3 = nn.Sequential(
-            spectral_norm(
-                nn.ConvTranspose2d(4 * self.conv_dim, 2 * self.conv_dim, 4, 2, 1)
-            ),
-            nn.BatchNorm2d(2 * self.conv_dim),
-            nn.ReLU(),
-        )
-
-        self.layer4 = nn.Sequential(
-            spectral_norm(
-                nn.ConvTranspose2d(2 * self.conv_dim, self.conv_dim, 4, 2, 1)
-            ),
-            nn.BatchNorm2d(self.conv_dim),
-            nn.ReLU(),
-        )
-
-        self.output = nn.Sequential(
-            nn.ConvTranspose2d(self.conv_dim, self.conv_dim, 4, 2, 1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(self.conv_dim, 3, 4, 2, 1),
-            nn.ReLU(),
-        )
-
-        self.attn1 = nn.MultiheadAttention(2 * self.conv_dim, num_heads=self.n_heads)
-        self.alpha = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        self.attn2 = nn.MultiheadAttention(self.conv_dim, num_heads=self.n_heads)
-        self.beta = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-
-    def forward(self, z):
-        z = z.view(z.size(0), z.size(1), 1, 1).contiguous()
-        out = self.layer1(z)
-        out = self.layer2(out)
-        out = self.layer3(out)
-
-        #### Attention 1 ####
-        identity = out
-        B, C, H, W = out.shape
-        q_1 = out.view(H * W, B, C)
-        k_1 = out.view(H * W, B, C)
-        v_1 = out.view(H * W, B, C)
-        out, attn_map_1 = self.attn1(q_1, k_1, v_1)
-        out = out.view(B, C, H, W)
-
-        # Residual connection
-        out = identity + self.alpha * out
-
-        out = self.layer4(out)
-
-        #### Attention 2 ####
-        identity = out
-        B, C, H, W = out.shape
-        q_2 = out.view(H * W, B, C)
-        k_2 = out.view(H * W, B, C)
-        v_2 = out.view(H * W, B, C)
-        out, attn_map_2 = self.attn2(q_2, k_2, v_2)
-        out = out.view(B, C, H, W)
-
-        out = self.output(out)
-
-        return torch.tanh(out), attn_map_1, attn_map_2
